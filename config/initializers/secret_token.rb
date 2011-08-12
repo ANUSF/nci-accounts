99842b5cc26e7e75bff8c772d31ec54a76f19834
Rohan Mitchell <rohan.mitchell@anu.edu.au>
2011-08-12 12:24:11 +1000

    Fresh rails app plus formtastic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NciAccounts::Application.config.secret_token = 'b785312b61496e3090ce22da8d422d90d88e07fdc5317b5ccb9d4c21ca5d03de7fe3c3895d788d51922fa30d57a3cf0c045087c9a6a2bf79d3dc41293a6385de'
